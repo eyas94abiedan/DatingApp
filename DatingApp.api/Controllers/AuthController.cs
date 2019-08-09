9a5b37e07acac371bbfe77a10d783b4c57a31ef2
eyas94abiedan <eyas.abiedan@hotmail.com>
2019-08-09 10:36:41 +0300

    End of Section 3

---
+++ DatingApp.api/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DatingApp.api.Data;
+using DatingApp.api.Dtos;
+using DatingApp.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo,IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto){
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.Username))
+            return BadRequest("User Already Exsists");
+
+            var UserToCreate = new User{
+                Username = userForRegisterDto.Username
+            };
+
+            var CreatedUser = await _repo.Register(UserToCreate,userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> login(UserForLoginDto userForLoginDto){
+
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),userForLoginDto.Password);
+            if(userFromRepo == null)
+                return Unauthorized();
+            
+            var claims = new []{
+                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config.GetSection("AppSetting:Token").Value));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                SigningCredentials=creds,
+                Subject=new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1)
+            };
+
+            var tokenHnadler = new JwtSecurityTokenHandler();
+
+            var token = tokenHnadler.CreateToken(tokenDescriptor);
+
+            return Ok( new{
+                token=tokenHnadler.WriteToken(token)
+            });
+        }
+    }
+}
--- DatingApp.api/Data/DataContext.cs
+++ DatingApp.api/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.api.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
 
         public DbSet<Values> Values { get; set; }
+        public DbSet<User> Users {get; set;}
     }
 }
